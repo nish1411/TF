2dd1f47ad38814f6d60c296c64ac50bae4266ee3
Singh <nishant.singh@softwareone.com>
2025-07-11 00:50:40 +0800

    ct

--- tf/ou.tf
+++ tf/ou.tf
@@ -1,9 +1,16 @@
 
+module "account" {
+  for_each = {
+    for acc in var.accounts :
+    acc.account_name => {
+      account_name  = acc.account_name
+      account_email = acc.account_email
+      ou_name       = lookup(acc, "ou_name", "")  # ✅ fallback if missing
+    }
+  }
 
-module "accounts" {
-  for_each          = { for acc in var.accounts : "${acc.account_name}" => acc }
-  source            = "../modules/account"
-  account_name      = each.value.account_name
-  account_email     = each.value.account_email
-  ou_name           = each.value.ou_name
+  source         = "../modules/account"
+  account_name   = each.value.account_name
+  account_email  = each.value.account_email
+  ou_name        = each.value.ou_name
 }
